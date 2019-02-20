695ae9cf6b97aa4e04cadeeb757fd5607b742c15
Matthew Brody <matt@Matthews-MacBook-Air-3.local>
2019-02-20 13:34:18 +1000

    Updates

---
+++ precache-manifest.308db0b885fc8c34119f6fb49fb26c91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f85c5f0dc57b4a09ad03",
+    "url": "/React-Youtube-API/static/js/runtime~main.f85c5f0d.js"
+  },
+  {
+    "revision": "10a2259ec9e2a9112578",
+    "url": "/React-Youtube-API/static/js/main.10a2259e.chunk.js"
+  },
+  {
+    "revision": "5918dbad8567dc79d88f",
+    "url": "/React-Youtube-API/static/js/2.5918dbad.chunk.js"
+  },
+  {
+    "revision": "10a2259ec9e2a9112578",
+    "url": "/React-Youtube-API/static/css/main.3bf21fc8.chunk.css"
+  },
+  {
+    "revision": "095be9b08df55994dd790a63270ef02f",
+    "url": "/React-Youtube-API/index.html"
+  }
+];
